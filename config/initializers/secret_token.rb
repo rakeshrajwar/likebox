52b9980df725b1127587ce2d02f9be2e0aef8ff7
Rakesh <rajwar@rajwar-HP-Pavilion-dv5-Notebook-PC.(none)>
2013-11-20 12:26:42 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Likebox::Application.config.secret_key_base = '5ec1ab23ff05b644464f77cdb17bcfd74f817670d020bd5ca0f0209dc37e818ca48b841b335b087754aea00dbb4e22e297fe33f3b66894aa57750cdaa277dc97'
